9dee20b53c0dc960d7eaa638a49c473271aea0d7
Adarsha <adarshamishra33@gmail.com>
2025-07-04 00:50:40 -0400

    vec and swaop

---
+++ src/bin/ex1.rs
@@ -0,0 +1,20 @@
+// #Given a list of integers, use a vector and return the median (when sorted, the value in the middle position)
+//  and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+
+fn main(){
+    let mut s = vec![3,4,1,2,10];
+    let mut counter;
+    for i in 0..s.len(){
+        for j in 0..s.len(){
+            if s[j] > s[i]{
+               counter = s[i];
+               s[i] = s[j];
+               s[j] = counter;
+            }
+        }
+    }
+
+    println!("{:?}",s)
+
+
+}
