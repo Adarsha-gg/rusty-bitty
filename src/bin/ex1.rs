a0af14c4cfa9aa9459704ae84045521df5085e91
Adarsha <adarshamishra33@gmail.com>
2025-07-07 19:34:54 -0400

    yaz

--- src/bin/ex1.rs
+++ src/bin/ex1.rs
@@ -1,9 +1,10 @@
-// #Given a list of integers, use a vector and return the median (when sorted, the value in the middle position)
-//  and mode (the value that occurs most often; a hash map will be helpful here) of the list.
 
+use std::{collections::HashMap, fs::exists, hash::Hash,};
 fn main(){
-    let mut s = vec![3,4,1,2,10];
+    let mut s = vec![3,4,1,2,10,1,66,0,0];
     let mut counter;
+    let mut largest_value=0;
+    let mut largest_key = 0;
     for i in 0..s.len(){
         for j in 0..s.len(){
             if s[j] > s[i]{
@@ -14,9 +15,27 @@ fn main(){
         }
     }
 
-    let median = s[2];
-    println!("Median is {}",median);
-    println!("{:?}",s)
+    let median = s[(s.len()/2)];
+    println!("Median is {median:?}",);
+    println!("{:?}",s);
 
+    let mut hashhy: HashMap<usize, u32>  = HashMap::new();
+    for num in s{
+        if hashhy.contains_key(&num)==false{
+            hashhy.insert(num, 0);
+        }
+        else {
+            let g =hashhy.get(&num);
+            let new_value = *g.unwrap() + 1;
+            hashhy.insert(num, new_value);
+            if largest_value < new_value {
+                largest_value = new_value;
+                largest_key = num;
+            }
+        }
+    }
+    println!("{largest_value:?}okk {largest_key:?}");
+    
+    
 
 }
